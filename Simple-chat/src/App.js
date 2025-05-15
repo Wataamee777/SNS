0ae047eec8197c85d105dd2b3a8e0fc85b9bf8ae
わたあめえ <Wataamee777@gmail.com>
2025-05-15 20:54:53 +0900

    Update App.js

--- Simple-chat/src/App.js
+++ Simple-chat/src/App.js
@@ -1,13 +1,15 @@
-import React from "react";
-import SimpleChat from "./SimpleChat";
+import React, { useState } from "react";
+import Auth from "./Auth";
+import Chat from "./Chat";
 
 function App() {
+  const [user, setUser] = useState(null);
+
   return (
     <div className="App">
-      <SimpleChat />
+      {user ? <Chat user={user} /> : <Auth onLogin={setUser} />}
     </div>
   );
 }
 
 export default App;
-
